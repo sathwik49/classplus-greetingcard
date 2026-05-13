58c4254230ffad227a5d3b22dbd8d7999a0915ca
Sathwik <sathwik2625@gmail.com>
2026-05-14 01:22:58 +0530

    fixed frontend origin issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: appConfig.FRONTEND_ORIGIN,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   }),
